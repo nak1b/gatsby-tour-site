613a87a6c4f527f90632de642a95020535cdecc5
Nakib Momin <nbmomin14@gmail.com>
2020-03-04 16:07:02 -0500

    added assets and layout.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import '../css/layout.css'
 
 const Layout = ({ children }) => {
   return (
