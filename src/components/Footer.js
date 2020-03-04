50e44b36319f25de435a6bd06f1e46c472845d10
Nakib Momin <nbmomin14@gmail.com>
2020-03-04 18:48:55 -0500

    added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import styles from '../css/footer.module.css'
+import navLinks from '../constants/Links'
+import socialLinks from '../constants/SocialLinks'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {navLinks.map((item, i) => (
+          <Link key={i} to={item.path}>
+            {item.text}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        {socialLinks.map((item, i) => (
+          <a key={i} href={item.url} target="_blank" rel="noopener noreferrer">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>
+        copyright backroad travel company {new Date().getFullYear()} All rights
+        reserved
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
