1e42be9e7b26c6521b4ecf8012d898b280679740
Nakib Momin <nbmomin14@gmail.com>
2020-03-03 23:49:15 -0500

    added pages

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error404() {
+  return (
+    <div>
+      <span>404 page</span>
+    </div>
+  )
+}
---
+++ src/pages/tours.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class tours extends Component {
+  render() {
+    return (
+      <div>
+        <span>Tours</span>
+      </div>
+    )
+  }
+}
