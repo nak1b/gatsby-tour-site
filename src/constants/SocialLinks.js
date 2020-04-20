24f30a23a7f3ba0686d8bfb1f921edd16e05237e
Nakib Momin <nbmomin14@gmail.com>
2020-04-19 23:21:27 -0400

    minor fixes

--- src/constants/SocialLinks.js
+++ src/constants/SocialLinks.js
@@ -4,14 +4,14 @@ import { FaFacebook, FaTwitterSquare, FaSquarespace } from 'react-icons/fa'
 export default [
   {
     icon: <FaFacebook />,
-    url: 'https//:www.google.com',
+    url: 'https://www.google.com',
   },
   {
     icon: <FaTwitterSquare />,
-    url: 'https//:www.google.com',
+    url: 'https://www.google.com',
   },
   {
     icon: <FaSquarespace />,
-    url: 'https//:www.google.com',
+    url: 'https://www.google.com',
   },
 ]
